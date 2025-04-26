8a4d38209e87dac39f01428ceeb1c4c5c3f10cc5
Dhru Viramgama <dhru@linearloop.io>
2025-04-26 15:13:47 +0530

    New changes

--- controller/contact.js
+++ controller/contact.js
@@ -1,68 +1,67 @@
 const nodemailer = require("nodemailer");
 
 const contactUs = async (req, res) => {
-    const { name, email, message } = req.body;
+  const { name, email, message } = req.body;
 
-    if (!name || !email || !message) {
-        return res.status(400).json({
-            message: 'Provide All Required Feilds',
-            ["exp."]: {
-                name: "string",
-                email: "string",
-                message: "string",
-            }
-        });
-    };
+  if (!name || !email || !message) {
+    return res.status(400).json({
+      message: "Provide All Required Felids",
+      ["exp."]: {
+        name: "string",
+        email: "string",
+        message: "string",
+      },
+    });
+  }
 
-    try {
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            host: "smtp.gmail.email",
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.GOOGLE_USERID,
-                pass: process.env.GOOGLE_PASSWORD,
-            },
-        });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.email",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.GOOGLE_USERID,
+        pass: process.env.GOOGLE_PASSWORD,
+      },
+    });
 
-        await transporter.sendMail({
-            from: '"QuizWiz App" <quizwiz@gmail.com>',
-            to: "dhru33170@gmail.com",
-            subject: "Alert Someone Contact You",
-            html: `
-            <div style="max-width: 600px; margin: 0 auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
-                <h2 style="margin-bottom: 20px; color: #333;">Contact Information</h2>
-                <div style="margin-bottom: 15px;">
-                    <span style="font-weight: bold; color: #666;">Name:</span>
-                    <span style="color: #333;">${name}</span>
-                </div>
-                <div style="margin-bottom: 15px;">
-                    <span style="font-weight: bold; color: #666;">Email:</span>
-                    <span style="color: #333;">${email}</span>
-                </div>
-                <div style="margin-bottom: 15px;">
-                    <span style="font-weight: bold; color: #666;">Message:</span>
-                    <p style="margin: 0; padding: 10px; border: 1px solid #ddd; border-radius: 4px; background-color: #f9f9f9; color: #333;">
-                        ${message}
-                    </p>
-                </div>
-            </div>
+    await transporter.sendMail({
+      from: '"QuizWiz App" <quizwiz@gmail.com>',
+      to: "dhru33170@gmail.com",
+      subject: "Alert Someone Contact You",
+      html: `
+      <div style="max-width: 600px; margin: 0 auto; background: #fff; padding: 20px; border-radius: 8px; box-shadow: 0 2px 5px rgba(0,0,0,0.1);">
+          <h2 style="margin-bottom: 20px; color: #333;">Contact Information</h2>
+          <div style="margin-bottom: 15px;">
+              <span style="font-weight: bold; color: #666;">Name:</span>
+              <span style="color: #333;">${name}</span>
+          </div>
+          <div style="margin-bottom: 15px;">
+              <span style="font-weight: bold; color: #666;">Email:</span>
+              <span style="color: #333;">${email}</span>
+          </div>
+          <div style="margin-bottom: 15px;">
+              <span style="font-weight: bold; color: #666;">Message:</span>
+              <p style="margin: 0; padding: 10px; border: 1px solid #ddd; border-radius: 4px; background-color: #f9f9f9; color: #333;">
+                  ${message}
+              </p>
+          </div>
+      </div>
         `,
-        });
+    });
 
-        return res.status(200).json({
-            message: 'Information Sent Successfully'
-        });
-
-    } catch (error) {
-        return res.status(500).json({
-            message: "Internal server error",
-            error,
-        });
-    };
+    return res.status(200).json({
+      message: "Information Sent Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
 };
 
 module.exports = {
-    contactUs
+  contactUs,
 };
--- index.js
+++ index.js
@@ -1,54 +1,95 @@
-const express = require('express');
-const cors = require('cors')
-const swaggerUi = require('swagger-ui-express');
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("./swagger_output.json");
-const { userRegisterValidation, userLoginValidation } = require('./middleware/userValidation');
-const { registerUser, loginUser, checkWhoIs, logOutUser, changePassword, forgotPassword, checkLink, ResetPassword } = require('./controller/auth');
-const { authenticate, changePasswordValidate, forgotPasswordValidation, ResetPasswordValidate } = require('./middleware/auth');
-const { getQuiz, getOneQuiz, addQuiz, updateQuiz, deleteQuiz, addQuestion, editQuestion, deleteQuestion } = require('./controller/quiz');
-const { validateRole } = require('./middleware/validateRole');
-const { addHistory, getHistory, getScore, getUsersData } = require('./controller/users');
-const { contactUs } = require('./controller/contact');
-require('dotenv').config();
-require('./database/db');
+const {
+  userRegisterValidation,
+  userLoginValidation,
+} = require("./middleware/userValidation");
+const {
+  registerUser,
+  loginUser,
+  checkWhoIs,
+  logOutUser,
+  changePassword,
+  forgotPassword,
+  checkLink,
+  ResetPassword,
+} = require("./controller/auth");
+const {
+  authenticate,
+  changePasswordValidate,
+  forgotPasswordValidation,
+  ResetPasswordValidate,
+} = require("./middleware/auth");
+const {
+  getQuiz,
+  getOneQuiz,
+  addQuiz,
+  updateQuiz,
+  deleteQuiz,
+  addQuestion,
+  editQuestion,
+  deleteQuestion,
+} = require("./controller/quiz");
+const { validateRole } = require("./middleware/validateRole");
+const {
+  addHistory,
+  getHistory,
+  getScore,
+  getUsersData,
+} = require("./controller/users");
+const { contactUs } = require("./controller/contact");
+require("dotenv").config();
+require("./database/db");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static('public'));
-app.use(cors())
+app.use(express.static("public"));
+app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 // auth routes
-app.post('/auth/register', userRegisterValidation, registerUser)
-app.post('/auth/login', userLoginValidation, loginUser)
-app.post('/auth/forgot-password', forgotPasswordValidation, forgotPassword)
-app.post('/auth/check-link', checkLink)
-app.post('/auth/reset-password', ResetPasswordValidate, ResetPassword)
-app.get('/auth/whoAmI', authenticate, checkWhoIs)
-app.post('/auth/logout', authenticate, logOutUser)
-app.post('/auth/changepassword', authenticate, changePasswordValidate, changePassword)
+app.post("/auth/register", userRegisterValidation, registerUser);
+app.post("/auth/login", userLoginValidation, loginUser);
+app.post("/auth/forgot-password", forgotPasswordValidation, forgotPassword);
+app.post("/auth/check-link", checkLink);
+app.post("/auth/reset-password", ResetPasswordValidate, ResetPassword);
+app.get("/auth/whoAmI", authenticate, checkWhoIs);
+app.post("/auth/logout", authenticate, logOutUser);
+app.post(
+  "/auth/changepassword",
+  authenticate,
+  changePasswordValidate,
+  changePassword
+);
 
 // quiz routes
-app.get('/quiz', authenticate, getQuiz)
-app.get('/quiz/:id', authenticate, getOneQuiz)
-app.post('/quiz/add', authenticate, validateRole, addQuiz)
-app.put('/quiz/:id', authenticate, validateRole, updateQuiz)
-app.delete('/quiz/:id', authenticate, validateRole, deleteQuiz)
-app.post('/quiz/addquestion/:id', authenticate, validateRole, addQuestion)
-app.put('/quiz/editquestion/:id', authenticate, validateRole, editQuestion)
-app.delete('/quiz/:quizId/deletequestion/:questionId', authenticate, validateRole, deleteQuestion)
+app.get("/quiz", getQuiz);
+app.get("/quiz/:id", authenticate, getOneQuiz);
+app.post("/quiz/add", authenticate, validateRole, addQuiz);
+app.put("/quiz/:id", authenticate, validateRole, updateQuiz);
+app.delete("/quiz/:id", authenticate, validateRole, deleteQuiz);
+app.post("/quiz/addquestion/:id", authenticate, validateRole, addQuestion);
+app.put("/quiz/editquestion/:id", authenticate, validateRole, editQuestion);
+app.delete(
+  "/quiz/:quizId/deletequestion/:questionId",
+  authenticate,
+  validateRole,
+  deleteQuestion
+);
 
 // users routes
-app.get('/user', authenticate, validateRole, getUsersData)
-app.post('/user/addhistory', authenticate, addHistory)
-app.get('/user/history', authenticate, getHistory)
-app.get('/user/getscore', authenticate, getScore)
+app.get("/user", authenticate, validateRole, getUsersData);
+app.post("/user/addhistory", authenticate, addHistory);
+app.get("/user/history", authenticate, getHistory);
+app.get("/user/getscore", authenticate, getScore);
 
 // contact routes
 app.post("/contact", contactUs);
 
 app.listen(PORT, () => {
-    console.log(`Server is runing on port: ${PORT}`);
+  console.log(`Server is runing on port: ${PORT}`);
 });
